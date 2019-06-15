ab4fab1f331577dc3e42c7261d9f544816eb7a41
Serge Sychov <sergesychov@MacBook-Air-Serge.local>
2019-06-15 22:53:23 +0300

    Change whole layout

--- ImageMaker/ViewController.swift
+++ ImageMaker/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var choosePicthureContainerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         choosePicthureContainerView.isHidden = true
