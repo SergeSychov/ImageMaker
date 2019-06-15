06082b004fa6c22f693c89f3e4edab86bd4e6746
Serge Sychov <sergesychov@MacBook-Air-Serge.local>
2019-06-15 15:47:21 +0300

    StoryBoard: Layout of load buttons Controller: set for load buttons text label adjustment

--- ImageMaker/ViewController.swift
+++ ImageMaker/ViewController.swift
@@ -10,8 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var takeFromPhotoButton: UIButton!
+    @IBOutlet weak var useCameraButton: UIButton!
+    @IBOutlet weak var loadByLinkButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //set right behavoiur of buttons
+        takeFromPhotoButton.titleLabel?.minimumScaleFactor = 0.3
+        takeFromPhotoButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        takeFromPhotoButton.titleLabel?.textAlignment = .center;
+        
+        useCameraButton.titleLabel?.minimumScaleFactor = 0.3
+        useCameraButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        useCameraButton.titleLabel?.textAlignment = .center;
+        
+        loadByLinkButton.titleLabel?.minimumScaleFactor = 0.3
+        loadByLinkButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        loadByLinkButton.titleLabel?.textAlignment = .center;
         // Do any additional setup after loading the view.
     }
 
