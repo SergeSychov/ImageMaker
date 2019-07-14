afa7ca2f4000efba11531fa2904409282e8150b2
Serge Sychov <sergesychov@MacBook-Air-Serge.local>
2019-07-14 23:11:48 +0300

    add Radian Indicator View Class

--- ImageMaker/IndicatorView.swift
+++ ImageMaker/IndicatorView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class IndicatorView: UIView {
+class LinearIndicatorView: UIView {
     
     let indicatorColor:UIColor? = nil
     let pieces = 25
@@ -98,6 +98,78 @@ class IndicatorView: UIView {
             patch.move(to: point1)
             patch.addArc(withCenter: circleCenter, radius: radius/2, startAngle: 0, endAngle: CGFloat(Double.pi * 2), clockwise: true)
         }
+        
+        return patch
+    }
+    
+    func hideSelfAnimated(){
+        UIView.animate(withDuration: 0.6) {
+            self.alpha = 0
+        }
+    }
+    
+    func showSelfAnimated(){
+        UIView.animate(withDuration: 0.6) {
+            self.alpha = 1
+        }
+    }
+}
+
+//---------------------------------------------------------------------------------------
+//RADIAN INDICATOR VIEW
+//_______________________________________________________________________________________
+class RadialIndicatorView: UIView {
+    
+    var indicatorColor:UIColor? = nil
+
+    var lineWidth:CGFloat = 1.00
+    
+    var readyPart = 0.00 {
+        didSet (newValue){
+            self.indicatorColor = UIColor.init(white: 1, alpha: CGFloat(newValue))
+            self.setNeedsDisplay()
+            if newValue == 1 {
+                hideSelfAnimated()
+            } else {
+                if self.alpha == 0 {
+                    showSelfAnimated()
+                }
+            }
+        }
+    }
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        drawInitialIndicatorWithContext(context: UIGraphicsGetCurrentContext()!, rct: rect)
+        // Drawing code
+    }
+    
+    
+    func drawInitialIndicatorWithContext(context: CGContext, rct: CGRect) {
+        
+        context.addPath(drawRadianPatchInRect(rct: rct, donePart: readyPart).cgPath)
+        context.setLineWidth(rct.width < rct.height ? rct.width / 6 : rct.height / 6) //lineWidth
+        context.setLineCap(.round)
+        context.setStrokeColor(indicatorColor?.cgColor ?? UIColor.white.cgColor)
+        if (readyPart == 1) {
+            context.drawPath(using: .stroke)
+            context.drawPath(using: .fill)
+        } else {
+            context.drawPath(using: .stroke)
+        }
+        
+    }
+    
+    func drawRadianPatchInRect(rct:CGRect, donePart:Double) -> UIBezierPath {
+    
+        let radiansReady = Double.pi * 2 * donePart
+        let squareSide = rct.width < rct.height ? rct.width : rct.height
+        let center = CGPoint(x: rct.size.width / 2, y: rct.size.height / 2)
+        let radius = squareSide / 3
+
+        let patch = UIBezierPath()
+        patch.addArc(withCenter: center, radius: radius, startAngle: CGFloat(Double.pi * 1.5), endAngle: CGFloat(Double.pi * 1.5 + radiansReady), clockwise: true)
         
         return patch
     }
