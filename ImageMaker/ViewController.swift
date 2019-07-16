959020d4da53c44bf12a75a4dc517b6b242cf801
Serge Sychov <sergesychov@MacBook-Air-Serge.local>
2019-07-16 21:44:48 +0300

    shareButtonTapped save to photo - change alert style to action sheet - looks better

--- ImageMaker/ViewController.swift
+++ ImageMaker/ViewController.swift
@@ -15,7 +15,7 @@ let tempPhotoName = "tempPhotoName.jpeg"
 let workImageURL = "WorkImageURL"
 
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDataSourcePrefetching, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UIPopoverPresentationControllerDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate, ResultImageObjDelegate {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate, ResultImageObjDelegate {
 
 
     @IBOutlet weak var inputImageView: UIImageView!
@@ -26,7 +26,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var progressRadialIndicatorView: RadialIndicatorView!
     
     var inputImageUrl: URL? //need only for save
-    //var resImgObjNamesStorage = [String]()
     var curentResObj:ResultImageObj?
     var resuableImageStorageCache = NSCache<NSString, UIImage>()
     
@@ -49,7 +48,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         }
     }
 
-    
     override func viewDidLoad() {
 
         choosePicthureContainerView.isHidden = true
@@ -252,20 +250,18 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             do {
                 let imageToShare = try UIImage(contentsOfFile: urlForFileNamed(resImgObjNamesStorage[cellectedCelIndexPatch!.row]).path)
                 
-                let alertController = UIAlertController(title: "Save image to photo library", message:nil, preferredStyle: .alert)
+                let alertController = UIAlertController(title: "Save image to photo library", message:nil, preferredStyle: .actionSheet)
                 
                 alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (UIAlertAction) in
                     UIImageWriteToSavedPhotosAlbum(imageToShare!, nil, nil, nil)
                 }))
                 
                 alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (UIAlertAction) in
-                    
                 }))
+                
                 present(alertController, animated: true, completion: nil)
                 
                 /* //Use activity
-                 
-                 
                  let imageToShare = try UIImage(contentsOfFile: urlForFileNamed(resImgObjNamesStorage[cellectedCelIndexPatch.row]).path)
                  let activityViewController = UIActivityViewController(activityItems: [imageToShare!], applicationActivities: nil)
                  activityViewController.popoverPresentationController?.sourceView = self.view
@@ -278,117 +274,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             }
         }
 
-    }
-    
-    //collection view data sourse
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if (resImgObjNamesStorage.count>0){
-            //print("Numbers of rows: ",resImgObjNamesStorage.count)
-            return resImgObjNamesStorage.count
-        } else {
-            return 0
-        }
-    }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "resultImgCell", for: indexPath) as! ImgCell
-        let resultObjName = resImgObjNamesStorage[indexPath.row]
-        let imgView = cell.contentView.viewWithTag(1) as! UIImageView
-        
-        if let image = resuableImageStorageCache.object(forKey: resultObjName as NSString){
-            imgView.image = image
-        } else {
-            imgView.image = nil
-            let activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
-            activityIndicator.frame = cell.contentView.bounds
-            cell.addSubview(activityIndicator)
-            activityIndicator.startAnimating()
-            
-            let size = imgView.bounds.size
-            DispatchQueue.global(qos: .userInitiated).async {
-                //if let image =  loadImage(imageName:resultObjName, size: size){
-                 let image =  loadImage(imageName:resultObjName, size: size)
-                    DispatchQueue.main.async {
-                        activityIndicator.stopAnimating()
-                        activityIndicator.removeFromSuperview()
-                        if image != nil {
-                            self.resuableImageStorageCache.setObject(image!, forKey:resultObjName as NSString)
-                            self.collectionOfResultImg.reloadItems(at: [indexPath])
-                        }
-                    }
-                }
-        }
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
-        let side = collectionView.bounds.height > collectionView.bounds.width ? collectionView.bounds.width : collectionView.bounds.height
-
-        for indexPath in indexPaths {
-            //print(indexPath)
-            let resultObjName = self.resImgObjNamesStorage[indexPath.row]
-            if  self.resuableImageStorageCache.object(forKey:resultObjName as NSString) == nil{
-                DispatchQueue.global(qos: .userInitiated).async {
-
-                    if let image =  loadImage(imageName: resultObjName, size: CGSize(width: side, height: side)){
-                        DispatchQueue.main.async {
-                            self.resuableImageStorageCache.setObject(image, forKey:resultObjName as NSString)
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    //collection view delegate
-    
-    /*
-    func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        //let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "resultImgCell", for: indexPath)
-       // cell.isSelected = true
-        return true
-    }
-    func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }*/
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "resultImgCell", for: indexPath)
-        cell.isSelected = true
-        resultImageView.image = loadImage(imageName: resImgObjNamesStorage[indexPath.row], size:resultImageView.bounds.size)
-    }
-    /*
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "resultImgCell", for: indexPath)
-        cell.isHighlighted = false
-        
-        //let choosedCellImageName = resImgObjNamesStorage[indexPath.row]
-        resultImageView.image = loadImage(imageName: resImgObjNamesStorage[indexPath.row], size:resultImageView.bounds.size)
-        //let resultObj = resImgObjNamesStorage[indexPath.row]
-        print("resultObj at index: ", indexPath.row, " hase name: ", resImgObjNamesStorage[indexPath.row])
-        
-    }
-     */
-    //collection view flow layout delegate
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let height = collectionView.frame.size.height-2.0
-        return CGSize(width: height, height: height)
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 1.0
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 1.0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left:100, bottom: 0, right: 0)
     }
     
     func changeLookOfChoosePictureContainer(){
@@ -517,24 +402,3 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
 }
 
-class ImgCell: UICollectionViewCell {
-    var indicatorView:RadialIndicatorView? = nil
-    var readyImg = 1.00 {
-        didSet (newValue) {
-            if newValue < 1.00 {
-
-                if indicatorView == nil {
-                    indicatorView = RadialIndicatorView(frame: self.bounds)
-                    indicatorView!.backgroundColor = UIColor.init(white: 0.5, alpha: 0.5)
-                    self.addSubview(indicatorView!)
-                }
-                indicatorView!.readyPart = newValue
-            } else {
-                if indicatorView != nil {
-                    indicatorView!.removeFromSuperview()
-                    indicatorView = nil
-                }
-            }
-        }
-    }
-}
