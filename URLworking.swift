809acd14641f712677c695c50ca18f843f206bc0
Serge Sychov <sergesychov@MacBook-Air-Serge.local>
2019-07-30 01:03:48 +0300

    set working with load indicator

--- URLworking.swift
+++ URLworking.swift
@@ -16,6 +16,7 @@ extension ViewController: URLSessionDelegate, URLSessionDownloadDelegate, UIText
         let session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
         
         // Don't specify a completion handler here or the delegate won't be called
+        self.loadIndicatorView.showSelfAnimated()
         session.downloadTask(with: url).resume()
     }
 
@@ -26,33 +27,31 @@ extension ViewController: URLSessionDelegate, URLSessionDownloadDelegate, UIText
                 if let imageUrl = saveImageData(data: data, imageName: tempPhotoName) as  URL? {
                     self.userDidChoosedNewImg(imageUrl)
                 } else {
-                    //error catchin URL
-                    print("Don't catch URL of source")
+                    self.showNotSuccesAlert()
                 }
+                self.loadIndicatorView.hideSelfAnimated()
             }
         } else {
+            DispatchQueue.main.async {
+                self.showNotSuccesAlert()
+                self.loadIndicatorView.hideSelfAnimated()
+            }
             session.invalidateAndCancel()
-            print ("Not right URL or not image")
-            //fatalError("Cannot load the image")
         }
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        
-        //only for test--------------------------------
-        let formatter = ByteCountFormatter()
-        formatter.allowedUnits = [.useKB, .useMB]
-        //let num = formatter.
-        let written = formatter.string(fromByteCount: totalBytesWritten)
-        let expected = formatter.string(fromByteCount: totalBytesWritten)
-        //print("Downloaded \(written) / \(expected)")
-        //print("Downloaded", Double(bitPattern: UInt64(totalBytesWritten)) / Double(bitPattern: UInt64(totalBytesExpectedToWrite)))
-        //------------------------------------------------------
         if totalBytesExpectedToWrite > 0 && totalBytesWritten < totalBytesExpectedToWrite{
             DispatchQueue.main.async {
-                print("Downloaded", Double(bitPattern: UInt64(totalBytesWritten)) / Double(bitPattern: UInt64(totalBytesExpectedToWrite)))
-                self.progressIndicatorView.readyPart = Double(bitPattern: UInt64(totalBytesWritten)) / Double(bitPattern: UInt64(totalBytesExpectedToWrite))
+                self.loadIndicatorView.readyPart = Double(bitPattern: UInt64(totalBytesWritten)) / Double(bitPattern: UInt64(totalBytesExpectedToWrite))
             }
         }
+    }
+    func showNotSuccesAlert(){
+        let alertcontroller = UIAlertController(title: "Can't load image by this link", message:"try another", preferredStyle: .alert)
+        present(alertcontroller, animated: true, completion: nil)
+        alertcontroller.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { (UIAlertAction) in
+            //self.changeLookOfChoosePictureContainer()
+        }))
     }
 }
